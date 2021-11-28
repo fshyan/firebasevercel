f6446128dcf7f03f52afa54306f87752241c8892
fshyan1 <60270375+fshyan1@users.noreply.github.com>
2021-11-27 18:08:32 -0700

    testing vercel

---
+++ firebase/index.js
@@ -0,0 +1,9 @@
+//Build The App Instace of Firebase
+import { initializeApp } from 'firebase/app';
+
+import firebaseConfig from "./firebaseConfig";
+
+const firebase = initializeApp(firebaseConfig);
+
+
+export default firebase
